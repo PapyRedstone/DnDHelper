4c2eb48c511bf097d7c1fc9cd54f252df3054af1
PapyRedstone <alexandre.febvre@pm.me>
2024-01-27 15:41:04 +0100

    revert tuto

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,8 @@
 #include <QApplication>
 #include <QLabel>
 #include "MainWindow.h"
-#include <iostream>
 
 int main(int argc, char** argv) {
-    std::cout << "AAAAAAAAAAAAAAAAAAAAAAAA\n";
-    std::cout << "Hello World" << std::endl;
     QApplication app(argc, argv);
     MainWindow mainWindow;
     mainWindow.show();
