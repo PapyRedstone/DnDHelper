cda33a64838d923d44d9c83063958895c348b281
PapyRedstone <alexandre.febvre@pm.me>
2024-01-27 15:18:37 +0100

    test

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,10 @@
 #include <QApplication>
 #include <QLabel>
 #include "MainWindow.h"
+#include <iostream>
 
 int main(int argc, char** argv) {
+    std::cout << "AAAAAAAAAAAAAAAAAAAAAAAA\n";
     QApplication app(argc, argv);
     MainWindow mainWindow;
     mainWindow.show();
