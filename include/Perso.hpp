3673b03a761d760fa38cce16b4bf5a5a1ddd6ee0
Kiliki12 <ordikilian@gmail.com>
2024-02-07 20:11:06 +0100

    erreur dans la gestion des fonctions void sur le set des caractéristique corrigée

--- include/Perso.hpp
+++ include/Perso.hpp
@@ -11,19 +11,19 @@ public:
     void setStrength(const int& newstrength);
     int getStrengthMod() const;
     int getDexterity() const;
-    void setDexterity() const;
+    void setDexterity(const int& newdexterity);
     int getDexterityMod() const;
     int getConstitution() const;
-    void setConstitution() const;
+    void setConstitution(const int& newconstitution);
     int getConstitutionMod() const;
     int getIntelligence() const;
-    void setIntelligence() const;
+    void setIntelligence(const int& newintelligence);
     int getIntelligenceMod() const;
     int getWisdom() const;
-    void setWisdom() const;
+    void setWisdom(const int& newwisdom);
     int getWisdomMod() const;
     int getCharisma() const;
-    void setCharisma() const;
+    void setCharisma(const int& newcharisma);
     int getCharismaMod() const;
 private:
     int strength, dexterity, constitution, intelligence, wisdom, charisma;
